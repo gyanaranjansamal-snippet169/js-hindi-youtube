a424b4bfbb9b537c8b07776e617c178838f368ab
gyanaranjansamal-snippet169 <happyhopper011@gmail.com>
2024-11-23 22:45:56 +0530

    Add files via upload
    adding variables

---
+++ .devcontainer/01_variables.js
@@ -0,0 +1,12 @@
+const accountId =155453
+let accountemail =  "gyana@goggle.com"
+var accountpassword ="12345"
+accountcity = "angul"
+let accountstate;
+// accountId = 6 
+/*  preferr not to use var bcz of issue in block scope and functional scope
+*/
+accountEmail = "hc@hc.com"
+accountpassword = "21212121"
+accountCity = "Bengaluru"
+console.table([accountId, accountemail , accountpassword , accountCity ,accountstate])
